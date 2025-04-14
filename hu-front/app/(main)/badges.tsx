540ee58af9b9688fe74d2effe183db17cedf0549
Niek <niek.wolbrink@student.hu.nl>
2025-04-14 11:13:56 +0200

    Badges pagina aangemaakt

---
+++ hu-front/app/(main)/badges.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import { Link, router } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+
+
+export default function Badges() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Username</Text>
+      <Text style={styles.text}>Badges</Text>
+      <Text style={styles.text}>Alle badges</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1F1F1F",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  text: {
+    fontSize: 16,
+    color: "white",
+  },
+});
