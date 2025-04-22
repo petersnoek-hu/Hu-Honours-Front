577075624416bb64eb287b9afaef8d75520c9b48
Niek <niek.wolbrink@student.hu.nl>
2025-04-22 15:47:24 +0200

    allbadges aangepast,

--- hu-front/app/(main)/allbadges.tsx
+++ hu-front/app/(main)/allbadges.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { BadgeList } from "../../components/user/BadgeList";
 
 export default function AllBadges() {
-    return (
-      <View className="flex-1 bg-gray-900 px-4 pt-10">
-        <Text className="text-white text-lg font-bold mb-4">Alle Badges</Text>
-        <BadgeList />
+  return (
+    <View className="flex-1 bg-gray-900">
+      <View className="flex items-left justify-between px-4 py-4">
+        <Text className="text-white text-lg font-bold">Alle Badges</Text>
       </View>
-    );
+      <ScrollView className="flex-1">
+        <BadgeList />
+      </ScrollView>
+    </View>
+  );
   }
