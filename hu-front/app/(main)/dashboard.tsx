1710093241e5c23ac23e873a8ca43a1485321a4b
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2025-04-30 21:53:31 +0200

    style: update layout for Nofiticatiebalk component

--- hu-front/app/(main)/dashboard.tsx
+++ hu-front/app/(main)/dashboard.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { SafeAreaView, Text, ScrollView, View } from "react-native";
 import Level from "../../components/dashboard/Level";
 import Divider from "../../components/dashboard/Divider";
+import Notification from "../../components/dashboard/Notificatiebalk";
 
 const Dashboard = () => {
   return (
     <SafeAreaView className="flex-1">
-      <ScrollView className="flex-col-reverse p-4">
+      
+      <Notification level={1}/>
+      <ScrollView className="flex-col-reverse p-4">\
         <Level level={3}/>
       <Divider challenge={1}/>
         <Level level={2}/>
--- hu-front/components/dashboard/Notificatiebalk.tsx
+++ hu-front/components/dashboard/Notificatiebalk.tsx
@@ -8,9 +8,12 @@ export default function NofitifcatieBalk({level}: NotificatieProps){
 
     return(
         <View>
-            <View>
-                <Text>Het volgende level wat je kan spelen</Text>
-                <Text>Level {level}</Text>
+            <View className="absolute top-0 left-0 right-0 bg-[#646568] py-4 px-8 flex-row items-center z-50">
+                <View className="w-16 h-16 bg-[#4B4C4E] rounded-full"/>
+                <View className="flex-1 ml-8">
+                    <Text className="color-white">Het volgende level wat je kan spelen</Text>
+                    <Text className="color-white font-semibold text-lg mt-2">Level {level}</Text>
+                </View>
             </View>
         </View>
     )
