ffda96a8ca1bf8194dcb03eed17c1db7833a1a3d
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2025-04-30 22:28:27 +0200

    feat: Add achievementbalk component
    Achievementbalk component en bijbehorende svg toegevoegd

--- hu-front/app/(main)/dashboard.tsx
+++ hu-front/app/(main)/dashboard.tsx
@@ -3,11 +3,12 @@ import { SafeAreaView, Text, ScrollView, View } from "react-native";
 import Level from "../../components/dashboard/Level";
 import Divider from "../../components/dashboard/Divider";
 import Notification from "../../components/dashboard/Notificatiebalk";
+import Achievements from "../../components/dashboard/Achievementbalk";
 
 const Dashboard = () => {
   return (
     <SafeAreaView className="flex-1">
-      
+      <Achievements streak={5} badges={1}/>
       <Notification level={1}/>
       <ScrollView className="flex-col-reverse p-4">\
         <Level level={3}/>
